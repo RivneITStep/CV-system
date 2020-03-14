597a6e4dbff4b159ea5b66f90c32f83b1105a055
Богдан Зыщук <bohdan.zyshchuk@zagravagames.com>
2020-03-14 16:55:08 +0200

    Add get Login data from Api

--- js/script.js
+++ js/script.js
@@ -3,3 +3,23 @@ $(document).ready(function (){
 $(".wrapper").toggleClass("active");
     });
 });
+
+
+const ul = document.getElementById("navbarDropdownUserName"); 
+const url = 'https://cvsystem.azurewebsites.net/api/adminData/get'; 
+
+
+fetch(url)
+.then(res => res.json())
+.then((out) => {
+  console.log('Checkout this JSON! ', out);
+  ul.innerHTML = out[0].login.loginName;
+})
+.catch(err => { throw err });
+
+ 
+
+
+
+
+// var obj = 
