49b625c5e857cfb0e8307fe90b5835c10235ec28
Владимир Лукащук <bartolomio177@gmail.com>
2020-03-31 22:28:41 +0300

    PUT was added to LoginCvBinderController

--- CVSystemAPI/Controllers/LoginCvBinderController.cs
+++ CVSystemAPI/Controllers/LoginCvBinderController.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace CVSystemAPI.Controllers
 {
@@ -30,7 +31,7 @@ namespace CVSystemAPI.Controllers
         #endregion
 
         #region POSTS
-        [HttpPost("post/register/{signInId}&{person}")]
+        [HttpPost("post/register/{signInId}")]
         public IActionResult PostRegister(int signInId, [FromBody] PersonalData person)
         {
             if (person == null || !ModelState.IsValid || signInId < 0) // if any is invalid - return BadRequest
@@ -55,6 +56,40 @@ namespace CVSystemAPI.Controllers
                     else
                     {
                         return BadRequest($"Person already exists in base!");
+                    }
+                }
+                else
+                {
+                    return BadRequest($"Login with ID={signInId} not found");
+                }
+            }
+        }
+        [HttpPut("put/updatePersonalData/{signInId}")]
+        public IActionResult PutRegister(int signInId, [FromBody] PersonalData person)
+        {
+            if (person == null || !ModelState.IsValid || signInId < 0) // if any is invalid - return BadRequest
+            {
+                return BadRequest($"Object Person is null. Or ModelState({ModelState.IsValid}) is invalid. Or signInId is invalid({signInId})");
+            }
+            using (CVContext db = new CVContext(OptionsHelper<CVContext>.GetOptions()))
+            {
+                var login = db.Signin.FirstOrDefault(x => x.LoginId.Equals(signInId)); //get login from db
+                if (login != null) // go on if exists
+                {
+                    var p = db.PersonalData.FirstOrDefault(x => x.PersonalId.Equals(person.PersonalId)); //get person from db
+                    if (p == null) // if none - BadRequest
+                    {
+                        return BadRequest($"Person does not exist in base!");
+                    }
+                    else
+                    {
+                        var l = db.LoginCvBinder.FirstOrDefault(x => x.LoginId.Equals(signInId));
+                        db.Entry(l).State = EntityState.Detached;
+                        l.Personal = UpdateHelper<PersonalData>.UpdateEntity(l.Personal, person);
+                        db.Entry(l).State = EntityState.Modified;
+                        db.SaveChanges();
+                        var lcvb = db.LoginCvBinder.FirstOrDefault(x => x.LoginId.Equals(signInId) && x.PersonalId.Equals(person.PersonalId));
+                        return CreatedAtAction(nameof(Get), lcvb); // return positive result
                     }
                 }
                 else
--- CVSystemAPI/Startup.cs
+++ CVSystemAPI/Startup.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CVSystemAPI.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -30,6 +32,7 @@ namespace CVSystemAPI
             services.AddControllersWithViews()
                 .AddNewtonsoftJson
                 (options => options.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore);
+            services.AddDbContext<CVContext>(c => c.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
