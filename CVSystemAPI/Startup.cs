76a200af7d37be538448d99f7ee33f4025875e28
Владимир Лукащук <bartolomio177@gmail.com>
2020-04-07 17:18:54 +0300

    CORS policy was added

--- CVSystemAPI/Startup.cs
+++ CVSystemAPI/Startup.cs
@@ -28,6 +28,7 @@ namespace CVSystemAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(c => c.AddPolicy("AllowAnyOrigin", options => options.AllowAnyOrigin()));
             services.AddControllers();
             services.AddControllersWithViews()
                 .AddNewtonsoftJson
@@ -38,10 +39,13 @@ namespace CVSystemAPI
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseCors();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
+
             app.UseHttpsRedirection();
 
             app.UseRouting();
